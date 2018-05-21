80b5143be24c9931dccc21a2c8e2af675e038a51
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-05-21 22:09:14 +0800

    Fix rubocop

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.configure do |config|
@@ -6,12 +8,14 @@ RSpec.configure do |config|
   # to ensure that it's confiugred to server Swagger from the same folder
   config.swagger_root = Rails.root.to_s + '/swagger'
 
-  # Define one or more Swagger documents and provide global metadata for each one
-  # When you run the 'rswag:specs:to_swagger' rake task, the complete Swagger will
-  # be generated at the provided relative path under swagger_root
+  # Define one or more Swagger documents and provide global metadata for each
+  # one
+  # When you run the 'rswag:specs:to_swagger' rake task, the complete Swagger
+  # will be generated at the provided relative path under swagger_root
   # By default, the operations defined in spec files are added to the first
-  # document below. You can override this behavior by adding a swagger_doc tag to the
-  # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
+  # document below. You can override this behavior by adding a swagger_doc tag
+  # to the the root example_group in your specs,
+  # e.g. describe '...', swagger_doc: 'v2/swagger.json'
   config.swagger_docs = {
     'v1/swagger.json' => {
       swagger: '2.0',
