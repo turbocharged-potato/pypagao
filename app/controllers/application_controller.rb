f2d8f97e7c9c9e830bf26083b0588c5267105fcd
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-05-22 01:26:01 +0800

    Fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:university_id, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[university_id name])
   end
 end
