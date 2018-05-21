1a1ee96b667f9ad7f9e96cea1c5f1b44379a2fd4
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-05-21 23:21:17 +0800

    Exclude config from rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@
 class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
   has_many :comments, dependent: :destroy
   has_many :answers, dependent: :destroy
