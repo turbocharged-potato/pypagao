8452cbc8529cd40b7beefb32c7f093901051b581
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-06-21 00:52:56 +0800

    Fix rubocop offence

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module Pypagao
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: %i[get post options]
       end
     end
   end
