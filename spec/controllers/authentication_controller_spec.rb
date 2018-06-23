b144d1f2746b1d94c9499d96a423c9f14f28974f
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-06-23 10:20:50 +0800

    Add login test

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -1,6 +1,27 @@
 # frozen_string_literal: true
 
+require 'jwt_token'
 require 'rails_helper'
 
 RSpec.describe AuthenticationController, type: :controller do
+  describe 'POST #login' do
+    it 'should gives token to valid users' do
+      user = create(:user)
+      post :login, params: { email: user.email, password: user.password }
+      should respond_with :ok
+      expect(JSON.parse(response.body)).to have_key('token')
+    end
+
+    it 'should return 401 for invalid users' do
+      post :login, params: { email: 'anu', password: 'asu' }
+      should respond_with :unauthorized
+      expect(JSON.parse(response.body)['error']).to eq('Wrong credentials')
+    end
+
+    it 'should return 422 for missing parameter(s)' do
+      post :login, params: { email: 'anu' }
+      should respond_with :unprocessable_entity
+      expect(JSON.parse(response.body)['error']).to eq('Missing parameter')
+    end
+  end
 end
