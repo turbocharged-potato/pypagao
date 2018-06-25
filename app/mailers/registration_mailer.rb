1a866a2c89d654e4ec68b07d6a54746b0b1a3d92
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-06-26 00:06:48 +0800

    Change verify url to be based on dotenv

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -5,7 +5,7 @@ class RegistrationMailer < ApplicationMailer
 
   def registration(user)
     @user = user
-    @confirmation_link = "http://new.indocomsoft.com/anu/#{user.token}"
+    @confirmation_link = "#{ENV['FRONTEND_VERIFY_URL']}?token=#{user.token}"
     mail(to: user.email, subject: 'Confirm your registration')
   end
 end
