6b599df76675891cdaeeecf3f272949ec9a4af8f
Tiffany Chong <sciffany@gmail.com>
2018-07-14 13:46:40 +0800

    Untrumpify

--- spec/controllers/semesters_controller_spec.rb
+++ spec/controllers/semesters_controller_spec.rb
@@ -9,23 +9,6 @@ RSpec.describe SemestersController, type: :controller do
   end
 
   describe 'GET #show' do
-    it 'should return all semesters given course id' do
-      semesters = create_list(:semesters, 3)
-      expected = semesters.each do |s|
-        { 'id' => s.id,
-          'end_year' => s.end_year,
-          'start_year' => s.start_year,
-          'number' => s.number}
-      end
-      get :index, params: { course_id: semester.course_id}
-
-    end
-    
-
-
-
-    end
-
   end
   describe 'POST #create' do
     it 'should save valid given information' do
@@ -46,7 +29,7 @@ RSpec.describe SemestersController, type: :controller do
                               start_year: semester.start_year,
                               number: semester.number }
       should respond_with :internal_server_error
-      expect(response.body).to eql({ error: 'Error saving'}.to_json)
+      expect(response.body).to eql({ error: 'Error saving' }.to_json)
     end
   end
 end
