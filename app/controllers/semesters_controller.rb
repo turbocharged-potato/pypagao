14e2603dd77f29dc8b94544219e28ad096e260b3
Tiffany Chong <sciffany@gmail.com>
2018-07-14 19:39:39 +0800

    modify controller and test for semester

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -1,27 +1,34 @@
 # frozen_string_literal: true
 
 class SemestersController < ApplicationController
-  skip_before_action :authenticate
-
   # /semesters?course_id=1 - lists all semester objects by course
   def index
     return unless ensure_params_fields([:course_id])
+
     semesters = Semester
                 .select(:id, :start_year, :end_year, :number, :course_id)
+                .joins(:course)
+                .where(courses: { university_id: current_user.university_id })
                 .where(course_id: params[:course_id])
     render_json(semesters, :ok)
   end
-
   def create
     return unless ensure_params_fields(%i[end_year start_year number])
-    if Semester.create(semester_params)
+    semester = Semester.new semester_params
+    if Course.find_by(id: semester[:course_id])
+             .university_id != current_user.university_id
+      render_error('University does not match current user', :bad_request)
+    elsif semester.save
       render_json('', :ok)
     else
-      render_error('Error saving', :internal_server_error)
+      render_error(semester.errors.full_messages.join(', '), :bad_request)
     end
   end
 
   def semester_params
-    params.require(:semester).permit(:start_year, :end_year, :number)
+    params.require(:semester).permit(:start_year,
+                                     :end_year,
+                                     :number,
+                                     :course_id)
   end
 end
