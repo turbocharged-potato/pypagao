0b05df22a43b95e8410fe5836cd9b5c7ab7f9672
Tiffany Chong <sciffany@gmail.com>
2018-07-14 22:47:19 +0800

    listing questions by paper

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,26 +1,48 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  skip_before_action :authenticate
-
   # /questions?paper_id=1 - lists all question objects by paper
+
   def index
     return unless ensure_params_fields([:paper_id])
-    questions_selected = Question.select(:id, :name, :paper_id)
-    questions = questions_selected.where(paper_id: params[:paper_id])
+    questions = Question.joins(paper: { semester: { course: :university } })
+                        .where(papers: { semesters:
+                                       { courses:
+                                       { universities:
+                                       { id: current_user.university_id } } } })
+                        .select(:id, :paper_id, :name)
+                        .where(paper_id: params[:paper_id])
     render_json(questions, :ok)
   end
 
   def create
     return unless ensure_params_fields([:name])
-    if Question.create(question_params)
+    question = Question.new question_params
+    if question_match_uni(question)
+      try_save_question(question)
+    else
+      render_error('University does not match current user', :bad_request)
+    end
+  end
+
+  private
+
+  def question_match_uni(question)
+    paper = Paper.find_by(id: question[:paper_id])
+    semester = Semester.find_by(id: paper[:semester_id])
+    course = Course.find_by(id: semester[:course_id])
+    course.university_id == current_user.university_id
+  end
+
+  def try_save_question(question)
+    if question.save
       render_json('', :ok)
     else
-      render_error('Error saving', :internal_server_error)
+      render_error(semester.errors.full_messages.join(', '), :bad_request)
     end
   end
 
   def question_params
-    params.require(:question).permit(:name)
+    params.require(:question).permit(:name, :paper_id)
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,28 +1,94 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
 RSpec.describe QuestionsController, type: :controller do
   before do
-    @request.env['HTTP_ACCEPT'] = 'application/json'
-    @request.env['CONTENT_TYPE'] = 'application/json'
+    @user_university = create(:university)
+    @user = create(:user, university: @user_university)
+    code = 'CS1101S'
+    @course = create(:course, code: code, university: @user_university)
+    @semester = create(:semester,
+                       course: @course)
+    @paper = create(:paper,
+                    semester: @semester)
   end
 
-  describe 'POST#create' do
-    it 'should save given valid information' do
-      question = build(:question)
-      post :create, params: { name: question.name,
-                              paper_id: question.paper_id }
-      should respond_with :ok
-      expect(response.body).to eql('')
+  describe '#GET index' do
+    before do
+      sign_in(@user)
     end
 
-    it 'should return 500 when error saving' do
+    it 'should list question by paper' do
+      question = create(:question, paper: @paper)
+      get :index, params: { paper_id: @paper.id }
+      should respond_with :ok
+      expect(JSON.parse(response.body))
+        .to eql([question_hash(question).with_indifferent_access])
+    end
+
+    it 'should not list papers with different paper id' do
+      paper = create(:paper, semester: @semester)
+      get :index, params: { paper_id: paper.id }
+      should respond_with :ok
+      expect(JSON.parse(response.body)).to eql([])
+    end
+
+    it 'should not list courses from different university' do
+      non_user_university = create(:university)
+      course = create(:course, university_id: non_user_university.id)
+      semester = create(:semester, course_id: course.id)
+      paper = create(:paper, semester_id: semester.id)
+      get :index, params: { paper_id: paper.id }
+      should respond_with :ok
+      expect(JSON.parse(response.body)).to eql([])
+    end
+  end
+
+  describe 'GET #index if signed out' do
+    it 'should not list any courses if signed out' do
+      get :index
+      should respond_with :unauthorized
+    end
+  end
+
+  describe 'POST #create' do
+    before do
+      sign_in(@user)
+    end
+
+    it 'should save given valid information' do
+      question = build(:question, paper: @paper)
+      post :create, params: question_hash(question)
+      should respond_with :ok
+      expect(response.body).to eq('')
+    end
+
+    it 'should not save information when university does not match' do
+      university = create(:university)
+      course = create(:course, university: university)
+      semester = create(:semester, course: course)
+      paper = create(:paper, semester: semester)
+      question = build(:question, paper: paper)
+      post :create, params: question_hash(question)
+      should respond_with :bad_request
+      expect(response.body)
+        .to eq({ error: 'University does not match current user' }.to_json)
+    end
+
+    it 'sends 400 when error saving' do
       question = build(:question)
       allow(Question).to receive(:create).and_return(false)
-      post :create, params: { name: question.name,
-                              paper_id: question.paper_id }
-      should respond_with :internal_server_error
-      expect(response.body).to eql({ error: 'Error saving' }.to_json)
+      post :create, params: question_hash(question)
+      should respond_with :bad_request
     end
+  end
+
+  private
+
+  def question_hash(question)
+    { id: question.id,
+      name: question.name,
+      paper_id: question.paper_id }
   end
 end
