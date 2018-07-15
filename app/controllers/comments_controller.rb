0030b5737dda4543c760b47375de9d8d1e1ac42a
Tiffany Chong <sciffany@gmail.com>
2018-07-15 15:20:48 +0800

    be singapore press aka create a good comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,45 @@
 # frozen_string_literal: true
 
-class QuestionsController < ApplicationController
-  skip_before_action :authenticate
-
+class CommentsController < ApplicationController
   # /comments?answers_id=1 - lists all comment objects by answer
   def index
     return unless ensure_params_fields([:answer_id])
-    comments_selected = Comment.select(:id, :content, :user_id, :answer_id)
-    comments = comments_selected.where(answer_id: params[:answer_id])
+    comments = Comment.joins(answer: { user: :university })
+                      .where(answers: { users:
+                                      { universities:
+                                      { id: current_user.university_id } } })
+                      .select(:id, :answer_id, :content, :user_id)
+                      .where(answer_id: params[:answer_id])
     render_json(comments, :ok)
   end
 
   def create
-    return unless ensure_params_fields(:content)
-    if Comment.create(comment_params)
+    return unless ensure_params_fields(%i[content answer_id])
+    comment = Comment.new comment_params
+    comment.user_id = current_user.id
+    if comment_match_uni(comment)
+      try_save_comment(comment)
+    else
+      render_error('University does not match current user', :bad_request)
+    end
+  end
+
+  private
+
+  def try_save_comment(comment)
+    if comment.save
       render_json('', :ok)
     else
-      render_error('Error saving', :internal_server_error)
+      render_error(comment.errors.full_messages.join(', '), :bad_request)
     end
   end
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :answer_id)
+  end
+
+  def comment_match_uni(comment)
+    comment.answer.question.paper.semester.course.university ==
+      current_user.university
   end
 end
